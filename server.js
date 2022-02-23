f8f413ec17dfb25651e83f810f4ff18fd665fae6
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-22 20:21:00 -0800

    configure model exports and requirements for user, thought, and reaction models

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.use(routes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
