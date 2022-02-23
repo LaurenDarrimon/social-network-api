a97571f32a41518f6278a3eff14c2ca144f72fb7
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-22 21:16:59 -0800

    create the structure of user schema and create many with seed data via mongoose

--- models/User.js
+++ models/User.js
@@ -1,6 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { 
+      type: String, 
+      required: [true, "Email required"],
+      unique: true, 
+      validate: {
+        validator: function(v) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(v);
+        },
+        message: "Please enter a valid email address"
+    },   
+    },
+  thoughts: [thoughtSchema],
+  friends: [userSchema],
+});
 
+const User = mongoose.model("User", userSchema);
 
+const handleError = (err) => console.error(err);
+
+// if the collection isn't empty, insert many Users
+User.find({}).exec((err, collection) => {
+  if (collection.length === 0) {
+    User.insertMany(
+      [
+        { username: "Aragorn", email: "king@gondor.gov" },
+        { username: "Frodo", email: "baggins@shire.com" },
+        { username: "Bilbo", email: "bilbo@shire.com" },
+      ],
+      (insertErr) => {
+        if (insertErr) {
+          handleError(insertErr);
+        }
+      }
+    );
+  }
+});
 
 module.exports = User;
