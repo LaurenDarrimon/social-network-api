feb6689c9d096d6845a79f1a6428390a4678f4de
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-22 22:15:15 -0800

    build models and schemas for thoughts and thier associated subdocument, reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,81 @@
+const { deepEqual } = require('assert');
 const mongoose = require('mongoose');
+const User = require('./User');
+
+
+// structure of thought subdocument
+const reactionSchema = new mongoose.Schema({
+    reactionId: mongoose.ObjectId, 
+    reactionBody: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    username: { 
+        type: String, 
+        required: true,
+        //type: mongoose.Schema.User.ObjectId,  
+     },
+});
+  
+// thoughtSchema is strcuture of the parent document
+const thoughtSchema = new mongoose.Schema({
+
+thoughtText: { type: String, required: true },
+createdAt: { type: Date, default: Date.now },
+username: { type: String, required: true },
+reactions: [reactionSchema],
+// This will include an array that holds all the reactions
+
+});
+  
+// Uses mongoose's .model() method to create the Thought model
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+// Uses model to create new instance of reaction subdocument
+const reactionData = [
+    { reactionBody: 'Me too!', username: "Aragorn" },
+    { reactionBody: 'That is deep, my friend.', username: "Frodo" },
+    { reactionBody: 'But will there be biscuits?', username: "Bilbo" },
+];
+
+
+const Thought = mongoose.model("User", thoughtSchema);
+
+const handleError = (err) => console.error(err);
+  
+// if the collection isn't empty, insert many thoughts
+Thought.find({}).exec((err, collection) => {
+
+    if (collection.length === 0) {
+        Thought.insertMany(
+        [
+            {
+            thoughtText: "My firends, you bow to no one.",
+            username: "Aragorn",
+            reactions: reactionData,
+            },
+            {
+            thoughtText: "I'm glad you're with me, @Sam.",
+            username: "Frodo",
+            reactions: reactionData,
+            },
+            {
+            thoughtText: "No admitance except on party business. ",
+            uername: "Bilbo",
+            reactions: reactionData,
+            },
+        ],
+        (insertErr) => {
+            if (insertErr) {
+            handleError(insertErr);
+            }
+        }
+        );
+    }
+});
+
+
+
+
+
 
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+const Thought = require('./Thought');
+
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
-  email: { 
-      type: String, 
-      required: [true, "Email required"],
-      unique: true, 
-      validate: {
-        validator: function(v) {
-            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(v);
-        },
-        message: "Please enter a valid email address"
-    },   
+  email: {
+    type: String,
+    required: [true, "Email required"],
+    unique: true,
+    validate: {
+      validator: function (inputEmail) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(inputEmail);
+      },
+      message: "Please enter a valid email address",
     },
+  },
   thoughts: [thoughtSchema],
   friends: [userSchema],
 });
@@ -26,9 +28,18 @@ User.find({}).exec((err, collection) => {
   if (collection.length === 0) {
     User.insertMany(
       [
-        { username: "Aragorn", email: "king@gondor.gov" },
-        { username: "Frodo", email: "baggins@shire.com" },
-        { username: "Bilbo", email: "bilbo@shire.com" },
+        {
+          username: "Aragorn",
+          email: "king@gondor.gov",
+        },
+        {
+          username: "Frodo",
+          email: "baggins@shire.org",
+        },
+        {
+          username: "Bilbo",
+          email: "bilbo@shire.org",
+        },
       ],
       (insertErr) => {
         if (insertErr) {
