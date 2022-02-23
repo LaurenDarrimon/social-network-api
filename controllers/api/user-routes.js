5ff5bc5f0a1376bcb79bef230b85ebfd8705dd58
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-22 22:30:56 -0800

    build routes for GET all users and GET one user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,8 +6,30 @@ const { Reaction, User, Thought } = require('./models');
 
 //GET all users
 
+router.get('/', (req, res) => {
+    // User model in the route to find all documents that are instances of that model
+    User.find({}, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
 
-//GET a single user by _id and populated thought and friend data
+
+//GET a single user by _id and populated with thought and friend data
+router.get('/:id', (req, res) => {
+    User.findOne({ id_: req.params.id }, (err, result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+      }
+    });
+  });
 
 
 //POST a new user
